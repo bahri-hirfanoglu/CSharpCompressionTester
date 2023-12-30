73fa9c0c97b4646a97cf299205efb10a70a187a2
bahri hrf <bahrihrf34@gmail.com>
2023-12-30 20:33:47 +0300

    add ICompressor interface for standardized compression/decompression methods

---
+++ App/ICompressor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCompression.Compressor
+{
+    public interface ICompressor
+    {
+        bool Compress(byte[] data, ref byte[] compressed);
+        bool Decompress(byte[] data, ref byte[] decompressed);
+    }
+
+}
