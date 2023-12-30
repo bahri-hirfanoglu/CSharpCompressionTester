5e9424118af3d677a4b84776defd2a3f823a2f0f
bahri hrf <bahrihrf34@gmail.com>
2023-12-30 20:50:57 +0300

    implement DeflateCompressor class for basic Deflate compression/decompression operations

---
+++ App/Compressor/DeflateCompressor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCompression.App.Compressor
+{
+    public class DeflateCompressor : ICompressor
+    {
+        public bool Compress(byte[] data, ref byte[] compressed)
+        {
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress))
+                {
+                    deflateStream.Write(data, 0, data.Length);
+                    deflateStream.Close();
+                    compressed = memoryStream.ToArray();
+                    return true;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Compression failed: " + ex.Message);
+                return false;
+            }
+        }
+
+        public bool Decompress(byte[] data, ref byte[] decompressed)
+        {
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream(data))
+                using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+                using (MemoryStream decompressedStream = new MemoryStream())
+                {
+                    deflateStream.CopyTo(decompressedStream);
+                    decompressed = decompressedStream.ToArray();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Decompression failed: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
