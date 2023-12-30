6f502609ca6e5a0254918f380a63f4b64717b763
bahri hrf <bahrihrf34@gmail.com>
2023-12-30 23:00:54 +0300

    edit README

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace CSharpCompression
+﻿using CSharpCompression.App;
+using CSharpCompression.App.Compressor;
+using CSharpCompression.Compressor;
+using System.Reflection;
+
+namespace CSharpCompression
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            byte[] testData = new byte[10000000];
+            new Random().NextBytes(testData);
+
+            var typesInAssembly = Assembly.GetExecutingAssembly().GetTypes();
+
+            var compressorTypes = typesInAssembly.Where(type => typeof(ICompressor).IsAssignableFrom(type) && !type.IsAbstract).ToList();
+
+            foreach (var type in compressorTypes)
+            {
+                var compressorInstance = (ICompressor)Activator.CreateInstance(type);
+                new TestCompressor(compressorInstance, testData, type.Name).RunTests();
+            }
+
+            Console.ReadKey();
         }
     }
 }
