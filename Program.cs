59d246f8cde62bad39020226318d9b4496dfee04
bahri hrf <bahrihrf34@gmail.com>
2023-12-30 20:31:07 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCompression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
