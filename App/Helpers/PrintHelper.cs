61a0d5c4862337c065646536939a0a4726124934
bahri hrf <bahrihrf34@gmail.com>
2023-12-30 23:34:58 +0300

    create PrintHelper

---
+++ App/Helpers/PrintHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCompression.App.Helpers
+{
+    public class PrintHelper
+    {
+        public static void PrintTableLine(string header, string value, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"{header.PadRight(30)} : ");
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+}
