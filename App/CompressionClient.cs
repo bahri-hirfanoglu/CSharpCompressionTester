ef58c0d64e3ee3d91556215b3e49b561c22058d7
bahri hrf <bahrihrf34@gmail.com>
2023-12-30 20:39:17 +0300

    implement CompressionClient class to utilize ICompressor for data compression and decompression

---
+++ App/CompressionClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCompression.Compressor
+{
+    public class CompressionClient
+    {
+        private ICompressor compressor;
+
+        public CompressionClient(ICompressor compressor)
+        {
+            this.compressor = compressor;
+        }
+
+        public bool Compress(byte[] inputData, out byte[] compressedData)
+        {
+            compressedData = null;
+            if (inputData == null || inputData.Length == 0)
+            {
+                Console.WriteLine("No data to compress.");
+                return false;
+            }
+
+            return compressor.Compress(inputData, ref compressedData);
+        }
+
+        public bool Decompress(byte[] inputData, out byte[] decompressedData)
+        {
+            decompressedData = null;
+            if (inputData == null || inputData.Length == 0)
+            {
+                Console.WriteLine("No data to decompress.");
+                return false;
+            }
+
+            return compressor.Decompress(inputData, ref decompressedData);
+        }
+    }
+}
