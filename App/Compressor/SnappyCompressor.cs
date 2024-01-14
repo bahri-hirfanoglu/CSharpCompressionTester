6b1a9b54fd504ab85bbb7354821ae38a43d9bce8
bahri hrf <bahrihrf34@gmail.com>
2024-01-14 23:24:28 +0300

    create snappy compressor

---
+++ App/Compressor/SnappyCompressor.cs
@@ -0,0 +1,47 @@
+﻿using CSharpCompression.Compressor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snappy.Sharp;
+
+namespace CSharpCompression.App.Compressor
+{
+    public class SnappyCompressor : ICompressor
+    {
+        public bool Compress(byte[] data, ref byte[] compressed)
+        {
+            try
+            {
+                var snappyCompressor = new Snappy.Sharp.SnappyCompressor();
+                int maxCompressedLength = snappyCompressor.MaxCompressedLength(data.Length);
+                compressed = new byte[maxCompressedLength];
+                int compressedLength = snappyCompressor.Compress(data, 0, data.Length, compressed);
+                Array.Resize(ref compressed, compressedLength);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Compression failed: " + ex.Message);
+                return false;
+            }
+        }
+
+        public bool Decompress(byte[] compressed, ref byte[] decompressed)
+        {
+            try
+            {
+                var snappyDecompressor = new Snappy.Sharp.SnappyDecompressor();
+                decompressed = snappyDecompressor.Decompress(compressed, 0, compressed.Length);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Decompression failed: " + ex.Message);
+                return false;
+            }
+        }
+    }
+
+}
